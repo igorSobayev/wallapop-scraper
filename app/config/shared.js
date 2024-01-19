53fbeaa65a84e990a6887bc1064d1c3fe0226a99
Igor <isobayev@gmail.com>
2024-01-19 20:20:21 +0100

    Add info to shared

--- app/config/shared.js
+++ app/config/shared.js
@@ -4,6 +4,19 @@ const config = {
         'USER': 'user',
         'ADMIN': 'admin',
     },
+    PLANS: {
+        'FREE': 'free',
+        'BASIC': 'basic',
+        'MEDIUM': 'medium',
+        'UNLIMITED': 'unlimited',
+    },
+    PLANS_CONFIG: {
+        'DAILY': 'daily',
+        'TWICE': 'twice',
+        'SIX_HOURS': 'six_hours',
+        'THREE_HOURS': 'three_hours',
+        'ONE_HOUR': 'one_hour',
+    },
     EXCLUDED_USER_FIELDS: '-password -email -role -deleted -__v',
     EXCLUDED_RECIPE_FIELDS: '-onProgress -deleted -deletedDate -deletedBy -deleted -__v',
 }
