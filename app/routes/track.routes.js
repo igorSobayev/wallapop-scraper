26d0080ec86888c327650d6840188a98c6f7746a
Igor <isobayev@gmail.com>
2024-01-20 17:23:54 +0100

    Upload tracks routes

--- app/routes/track.routes.js
+++ app/routes/track.routes.js
@@ -1,5 +1,15 @@
+// Middlewares
+import authJwt from '../middlewares/authJwt.js'
+
+// Controllers
+import upload from '../controllers/tracks/tracks.upload.controller.js'
+
+// Others
 import express from 'express'
 
 const router = express.Router()
+
+// router.get('/all/:user', [authJwt.verifyUserAndToken], deleteFiles)
+router.post('/upload', [authJwt.verifyUserAndToken], upload)
 
 export default router
