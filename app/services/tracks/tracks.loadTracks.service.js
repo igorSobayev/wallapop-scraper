2c1379d6359d13f78b502f98d357e9d8cd870852
Igor <isobayev@gmail.com>
2024-01-21 20:11:37 +0100

    Change loadTracks service to add the history element

--- app/services/tracks/tracks.loadTracks.service.js
+++ app/services/tracks/tracks.loadTracks.service.js
@@ -1,4 +1,5 @@
 import VError from 'verror'
+import mongoose from 'mongoose'
 
 import UserModel from '../../repository/users/user.model.js'
 import TrackModel from '../../repository/tracks/track.model.js'
@@ -14,7 +15,33 @@ export default async function loadTracks({ userId }) {
         throw VError('User is missing or deleted')
     }
 
-    const tracks = await TrackModel.find({ user: userId, deleted: false })
+    // Agregado para obtener Tracks con su último historial
+    const tracks = await TrackModel.aggregate([
+        {
+            $match: {
+                user: new mongoose.Types.ObjectId(userId),
+                deleted: false,
+            }
+        },
+        {
+            $lookup: {
+                from: 'trackhistories',
+                localField: '_id',
+                foreignField: 'trackId',
+                as: 'historial'
+            }
+        },
+        {
+            $addFields: {
+                lastElement: { $arrayElemAt: ['$historial', -1] }
+            }
+        },
+        {
+            $project: {
+                historial: 0
+            }
+        }
+    ])
 
     return tracks
 }
