926b6ee178943ac3f8946bc111fc35a4e45de557
Igor <isobayev@gmail.com>
2024-01-21 20:11:21 +0100

    Update tracks info controller

--- app/routes/track.routes.js
+++ app/routes/track.routes.js
@@ -4,6 +4,7 @@ import authJwt from '../middlewares/authJwt.js'
 // Controllers
 import upload from '../controllers/tracks/tracks.upload.controller.js'
 import loadTracks from '../controllers/tracks/tracks.loadTracks.controller.js'
+import updateTracksInfo from '../controllers/tracks/tracks.updateTracksInfo.controller.js'
 
 
 // Others
@@ -13,5 +14,6 @@ const router = express.Router()
 
 router.get('/all/:user', [authJwt.verifyUserAndToken], loadTracks)
 router.post('/upload', [authJwt.verifyUserAndToken], upload)
+router.post('/update-tracks-info', [authJwt.verifyUserAndToken], updateTracksInfo)
 
 export default router
