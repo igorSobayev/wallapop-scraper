106f40acc97e31d71ad323091cb177c5be65cfa1
Igor <isobayev@gmail.com>
2024-01-21 10:54:22 +0100

    Load tracks route

--- app/routes/track.routes.js
+++ app/routes/track.routes.js
@@ -3,13 +3,15 @@ import authJwt from '../middlewares/authJwt.js'
 
 // Controllers
 import upload from '../controllers/tracks/tracks.upload.controller.js'
+import loadTracks from '../controllers/tracks/tracks.loadTracks.controller.js'
+
 
 // Others
 import express from 'express'
 
 const router = express.Router()
 
-// router.get('/all/:user', [authJwt.verifyUserAndToken], deleteFiles)
+router.get('/all/:user', [authJwt.verifyUserAndToken], loadTracks)
 router.post('/upload', [authJwt.verifyUserAndToken], upload)
 
 export default router
