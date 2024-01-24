3bab11693f86e9af83981630bcefecac161f232c
Igor <isobayev@gmail.com>
2024-01-24 20:13:00 +0100

    Archive and delete track routes

--- app/routes/track.routes.js
+++ app/routes/track.routes.js
@@ -5,6 +5,8 @@ import authJwt from '../middlewares/authJwt.js'
 import upload from '../controllers/tracks/tracks.upload.controller.js'
 import loadTracks from '../controllers/tracks/tracks.loadTracks.controller.js'
 import updateUserTracksInfo from '../controllers/tracks/tracks.updateUserTracksInfo.controller.js'
+import deleteTrack from '../controllers/tracks/tracks.deleteTrack.controller.js'
+import archiveTrack from '../services/tracks/tracks.archiveTrack.service.js'
 
 
 // Others
@@ -15,5 +17,7 @@ const router = express.Router()
 router.get('/all/:user', [authJwt.verifyUserAndToken], loadTracks)
 router.post('/upload', [authJwt.verifyUserAndToken], upload)
 router.post('/update-user-tracks-info', [authJwt.verifyUserAndToken], updateUserTracksInfo)
+router.delete('/delete/:id', [authJwt.verifyUserAndToken], deleteTrack)
+router.post('/archive/:id', [authJwt.verifyUserAndToken], archiveTrack )
 
 export default router
