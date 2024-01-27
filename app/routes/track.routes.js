fb0718fa8e27d9dfe26063d6b948b42be2e7e9b9
Igor <isobayev@gmail.com>
2024-01-27 10:35:38 +0100

    Sync track route

--- app/routes/track.routes.js
+++ app/routes/track.routes.js
@@ -7,6 +7,7 @@ import loadTracks from '../controllers/tracks/tracks.loadTracks.controller.js'
 import updateUserTracksInfo from '../controllers/tracks/tracks.updateUserTracksInfo.controller.js'
 import deleteTrack from '../controllers/tracks/tracks.deleteTrack.controller.js'
 import archiveTrack from '../controllers/tracks/tracks.archiveTrack.controller.js'
+import syncTrack from '../controllers/tracks/tracks.syncTrack.controller.js'
 
 
 // Others
@@ -18,6 +19,7 @@ router.get('/all/:user', [authJwt.verifyUserAndToken], loadTracks)
 router.post('/upload', [authJwt.verifyUserAndToken], upload)
 router.post('/update-user-tracks-info', [authJwt.verifyUserAndToken], updateUserTracksInfo)
 router.delete('/delete/:id', [authJwt.verifyUserAndToken], deleteTrack)
-router.put('/archive/:id', [authJwt.verifyUserAndToken], archiveTrack )
+router.put('/archive/:id', [authJwt.verifyUserAndToken], archiveTrack)
+router.put('/sync-track/:id', [authJwt.verifyUserAndToken], syncTrack)
 
 export default router
