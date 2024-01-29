4b3f35face1a625d87edc0fad1a1bf5538f72965
Igor <isobayev@gmail.com>
2024-01-29 19:19:19 +0100

    Add test uri

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,14 @@
 import db from './db.js'
 export async function init() {
   try {
-    await db.mongoose.connect(process.env.DB_URI, {
+
+    let uri = process.env.DB_URI
+
+    if (process.env.NODE_ENV === 'test') { // TODO no funca
+      uri = process.env.DB_URI_TEST
+    }
+
+    await db.mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
