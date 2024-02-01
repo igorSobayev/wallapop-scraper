d36994eda2ceeb9017203fd288f3e7fcbf20f64a
Igor <isobayev@gmail.com>
2024-02-01 23:37:44 +0100

    Update the service and controller on create session

--- app/controllers/users/users.createCheckoutSession.controller.js
+++ app/controllers/users/users.createCheckoutSession.controller.js
@@ -1,18 +1,18 @@
 import Utils from './../../utils/index.js'
-import UserResetPasswordService from './../../services/users/users.resetPassword.service.js'
+import CreateCheckoutSessionService from '../../services/users/payments/users.createCheckoutSession.service.js'
 
 export default async function createCheckoutSession (req, res) {
     try {
       let errors = Utils.validateRequest(req, ({ body, params }) => {
-        body('plans').required()
+        body('plan').required()
         params('id').isString().required()
       })
 
       if (errors) next(errors)
 
-      await UserResetPasswordService(resetPasswordInfo)
+      const session = await CreateCheckoutSessionService({ userId: req.params.id, plan: req.body.plan })
 
-      return res.status(200).send({ message: 'Password updated' })
+      return res.status(200).send(session)
     } catch (err) {
       res.status(500).send({ message: err.message })
     }
--- app/services/users/payments/users.createCheckoutSession.service.js
+++ app/services/users/payments/users.createCheckoutSession.service.js
@@ -1,32 +1,43 @@
 import VError from 'verror'
 import Stripe from 'stripe'
 
+import shared from './../../../config/shared.js'
+
 import UserModel from '../../../repository/users/user.model.js'
 
 const stripe = new Stripe(process.env.STRIPE_KEY)
 
-export default async function createCheckoutSession ({ userId, plans }) {
+export default async function createCheckoutSession ({ userId, plan }) {
     if (!userId) {
-        throw VError('userId is missing')
+      throw VError('userId is missing')
+    }
+
+    if (!shared.PLANS.includes(plan)) {
+      throw VError(`The plan ${plan} is not valid`)
     }
 
     const user = await UserModel.findOne({ _id: userId, deleted: false })
 
     if (!user) {
-        throw VError(`User with ID ${userId} not exist or is deleted`)
+      throw VError(`User with ID ${userId} not exist or is deleted`)
     }
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}/success.html`,
-        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-    })
+    const sessionData = {
+      line_items: [
+        {
+          ...shared.PLANS_DETAILS[plan]
+        },
+      ],
+      mode: 'payment',
+      success_url: `${env.process.DOMAIN_URL}/success.html`,
+      cancel_url: `${env.process.DOMAIN_URL}/cancel.html`,
+    }
 
+    if (user.customerId) {
+      sessionData.customer = user.customerId
+    }
+
+    const session = await stripe.checkout.sessions.create(sessionData)
+
+    return session
 }
