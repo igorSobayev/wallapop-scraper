b8f90efee81411cddf2d82afc616cf95d6b23cce
Igor <isobayev@gmail.com>
2024-02-03 17:36:23 +0100

    Add return page on controller

--- app/controllers/users/users.createCheckoutSession.controller.js
+++ app/controllers/users/users.createCheckoutSession.controller.js
@@ -1,16 +1,17 @@
 import Utils from './../../utils/index.js'
 import CreateCheckoutSessionService from '../../services/users/payments/users.createCheckoutSession.service.js'
 
-export default async function createCheckoutSession (req, res) {
+export default async function createCheckoutSession (req, res, next) {
     try {
       let errors = Utils.validateRequest(req, ({ body, params }) => {
         body('plan').required()
+        body('returnPage').required()
         params('id').isString().required()
       })
 
       if (errors) next(errors)
 
-      const session = await CreateCheckoutSessionService({ userId: req.params.id, plan: req.body.plan })
+      const session = await CreateCheckoutSessionService({ userId: req.params.id, plan: req.body.plan, returnPage: req.body.returnPage })
 
       return res.status(200).send(session)
     } catch (err) {
