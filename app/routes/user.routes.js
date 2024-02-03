089884f98a0f57bdec5f757e80acf044d2611004
Igor <isobayev@gmail.com>
2024-02-03 17:35:35 +0100

    Create checkout route

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,6 +5,7 @@ import edit from '../controllers/users/users.edit.controller.js'
 import uploadFile from '../controllers/users/users.uploadFile.controller.js'
 import uploadMultipleFiles from '../controllers/users/users.uploadMultipleFiles.controller.js'
 import deleteFiles from '../controllers/users/users.deleteFiles.controller.js'
+import createCheckoutSession from '../controllers/users/users.createCheckoutSession.controller.js'
 
 import express from 'express'
 import multer from 'multer'
@@ -20,7 +21,7 @@ router.post('/upload-file/:id', [authJwt.verifyUserAndToken], uploader.single('f
 router.post('/upload-multiple-files/:id', [authJwt.verifyUserAndToken], uploader.array('file[]'), uploadMultipleFiles)
 router.post('/delete-files/:id', [authJwt.verifyUserAndToken], deleteFiles)
 
-router.post('/create-checkout-session/:id', [authJwt.verifyUserAndToken], deleteFiles)
+router.post('/create-checkout-session/:id', [authJwt.verifyUserAndToken], createCheckoutSession)
 
 // Public endpoints
 router.put('/public/:username', publicDetails)
