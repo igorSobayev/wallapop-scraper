9f4dfc146a436f65c812ad4883eaa92485af3684
Igor <isobayev@gmail.com>
2024-02-10 12:47:36 +0100

    Registered user verification

---
+++ app/controllers/users/users.signupCode.controller.js
@@ -0,0 +1,21 @@
+import Utils from '../../utils/index.js'
+import SignupCodeService from '../../services/users/auth/users.signupCode.service.js'
+
+export default async function signupCode (req, res) {
+    try {
+      let errors = Utils.validateRequest(req, ({ query }) => {
+        query('username').isString().required()
+        query('code').isString().required()
+      })
+
+      if (errors) next(errors)
+
+      const { username, code } = req.query
+
+      await SignupCodeService({ username, code })
+
+      return res.status(200).send({ message: 'User verified successfully!' })
+    } catch (err) {
+      res.status(400).send({ name: err.name, message: err.message })
+    }
+  }
---
+++ app/services/users/auth/users.signupCode.service.js
@@ -0,0 +1,32 @@
+import VError from 'verror'
+
+import UserModel from '../../../repository/users/user.model.js'
+
+export default async function signupCode ({ username, code }) {
+    if (!username) {
+        throw VError('username is missing')
+    }
+
+    if (!code) {
+        throw VError(`code for user ${username} is missing`)
+    }
+
+    const user = await UserModel.findOne({ username })
+
+    if (!user) {
+        throw VError(`User with username ${username} is missing`)
+    }
+
+    if (user.active) {
+        throw VError({ name: 'AlreadyActiveError' }, `User with username ${username} is already active`)
+    }
+
+    if (user.registerCode !== code) {
+        throw VError({ name: 'InvalidCodeError' }, `User with username ${username} are using invalid code ${code}`)
+    }
+
+    user.active = true
+    user.verificationDate = new Date()
+
+    await user.save()
+}
